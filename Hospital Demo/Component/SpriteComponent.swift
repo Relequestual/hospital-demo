aad54e8584af108bb2a2ddec80ca3e2556d346c1
Relequestual <relequestual@gmail.com>
2015-12-22 08:18:52 +0000

    Created entity and component

---
+++ Hospital Demo/Component/SpriteComponent.swift
@@ -0,0 +1,9 @@
+//
+//  SpriteComponent.swift
+//  Hospital Demo
+//
+//  Created by Ben Hutton on 19/12/2015.
+//  Copyright © 2015 Ben Hutton. All rights reserved.
+//
+
+import Foundation
---
+++ Hospital Demo/Entity/Tile.swift
@@ -0,0 +1,9 @@
+//
+//  Tile.swift
+//  Hospital Demo
+//
+//  Created by Ben Hutton on 19/12/2015.
+//  Copyright © 2015 Ben Hutton. All rights reserved.
+//
+
+import Foundation
