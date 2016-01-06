69e9717e6a227282da85848182e0dba2beaf0bbb
Relequestual <relequestual@gmail.com>
2016-01-06 08:26:22 +0000

    Added more debug sprites

--- Hospital Demo/Component/SpriteDebugComponent.swift
+++ Hospital Demo/Component/SpriteDebugComponent.swift
@@ -12,14 +12,32 @@ import SpriteKit
 class SpriteDebugComponent: GKComponent {
 
   init(node: SKSpriteNode ) {
+    super.init()
     let size = node.texture?.size()
+    let position = entity?.componentForClass(PositionComponent)
     print(size)
 
     let debugRect = SKShapeNode(rectOfSize: size!)
     debugRect.strokeColor = UIColor.cyanColor()
     debugRect.zPosition = 100
     node.addChild(debugRect)
-    
+
+    let anchorPoint = SKShapeNode(circleOfRadius: 2.0)
+    anchorPoint.fillColor = UIColor.redColor()
+    anchorPoint.zPosition = 100
+    node.addChild(anchorPoint)
+
+
+    let desc = SKLabelNode(text: NSStringFromCGPoint(CGPoint(x: position!.x, y: position!.y)))
+    desc.fontColor = UIColor.blackColor()
+    desc.zPosition = 100
+    desc.position = CGPoint(x: 0, y: 0)
+    desc.fontSize = 10
+
+    desc.verticalAlignmentMode = .Top
+
+    node.addChild(desc)
+
   }
 
 }
