dd8cd8abc6792f512095dbf1d9b665fa1451ba4a
Relequestual <relequestual@gmail.com>
2016-02-05 07:51:31 +0000

    Added Game singleton class

---
+++ Hospital Demo/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Hospital Demo
+//
+//  Created by Ben Hutton on 04/02/2016.
+//  Copyright © 2016 Ben Hutton. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+  //  http://krakendev.io/blog/the-right-way-to-write-a-singleton
+  static let sharedInstance = Game()
+
+  private init() {}
+
+  
+}
