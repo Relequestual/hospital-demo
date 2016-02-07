348b967ddd7652cbb6d12a1af05d3a94b6b6b7cc
Relequestual <relequestual@gmail.com>
2016-02-07 23:34:17 +0000

    Added Build States

---
+++ Hospital Demo/BuildState.swift
@@ -0,0 +1,28 @@
+//
+//  BuildState.swift
+//  Hospital Demo
+//
+//  Created by Ben Hutton on 07/02/2016.
+//  Copyright © 2016 Ben Hutton. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+class BuildState : GKState{
+
+
+}
+
+
+class BSNoBuild: BuildState {
+
+}
+
+class BSPlaceItem: BuildState {
+
+}
+
+class BSSelectSqaures: BuildState {
+
+}
