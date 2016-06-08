85a14985739d5ea563697ec83f14a5df6bbac70f
Hugo Jobling <hello@thisishugo.com>
2016-06-08 23:24:42 +0100

    refactor GameToolbar

--- Hospital Demo/SceneElements/GameToolbar.swift
+++ Hospital Demo/SceneElements/GameToolbar.swift
@@ -12,61 +12,63 @@ import GameplayKit
 import HLSpriteKit
 
 class GameToolbar: HLToolbarNode {
-
-  let options = [
-    "red": { GameToolbar.redTouch() },
-    "green": { GameToolbar.greenTouch() },
-    "blue": { GameToolbar.blueTouch() }
-  ]
-
-
+  
+  enum Option: String {
+    case Red, Green, Blue
+  }
+  
   init(size: CGSize, baseScene: BaseScene) {
     super.init()
-
+    
     self.anchorPoint = CGPoint(x: 0, y: 0)
     self.size = size
     self.position = CGPoint(x: 0, y: 0)
     self.zPosition = 999
-
-    let toolNodes = NSMutableArray()
-
+    
     let redTool = SKSpriteNode(color: UIColor.redColor(), size: CGSize(width: 20, height: 20))
     let greenTool = SKSpriteNode(color: UIColor.greenColor(), size: CGSize(width: 20, height: 20))
     let blueTool = SKSpriteNode(color: UIColor.blueColor(), size: CGSize(width: 20, height: 20))
+    
+    let toolNodes = [redTool, greenTool, blueTool]
+    let tags = [Option.Red.rawValue, Option.Green.rawValue, Option.Blue.rawValue]
+    self.setTools(toolNodes, tags: tags, animation:HLToolbarNodeAnimation.SlideUp)
+    
+    self.toolTappedBlock = {(tag: String!) -> Void in
+      baseScene.HL_showMessage("Tapped \(tag) tool on HLToolbarNode.")
 
-
-    toolNodes.addObject(redTool)
-    toolNodes.addObject(greenTool)
-    toolNodes.addObject(blueTool)
-    self.setTools(toolNodes as [AnyObject], tags: ["red", "green", "blue"], animation:HLToolbarNodeAnimation.SlideUp)
-
-    self.toolTappedBlock = {(toolTag: String!) -> Void in
-      baseScene.HL_showMessage("Tapped \(toolTag) tool on HLToolbarNode.")
-
-      if let closure = self.options[toolTag] {
-        closure()
-      }
-
-//      Game.sharedInstance.stateMachine.enterState(GeneralState)
+      GameToolbar.didTouch(tag)
+      //Game.sharedInstance.stateMachine.enterState(GeneralState)
     }
-
-    //    self.registerDescendant(toolbarNode, withOptions: Set<AnyObject>.setWithObject(HLSceneChildGestureTarget))
-//    baseScene.registerDescendant(self, withOptions: Set(arrayLiteral: HLSceneChildGestureTarget))
-
+    
+    //self.registerDescendant(toolbarNode, withOptions: Set<AnyObject>.setWithObject(HLSceneChildGestureTarget))
+    //baseScene.registerDescendant(self, withOptions: Set(arrayLiteral: HLSceneChildGestureTarget))
+    
   }
-
+  
   required init?(coder aDecoder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
+    fatalError("init(coder:) has not been implemented")
   }
-
+  
+  class func didTouch(tag: String) {
+    guard let option = Option(rawValue: tag) else { return }
+    switch option {
+    case .Red:
+      GameToolbar.redTouch()
+    case .Green:
+      GameToolbar.greenTouch()
+    case .Blue:
+      GameToolbar.blueTouch()
+    }
+  }
+  
   class func redTouch() {
     Game.sharedInstance.gameStateMachine.enterState(GSLevelEdit)
   }
-
+  
   class func greenTouch() {
     Game.sharedInstance.gameStateMachine.enterState(GSGeneral)
   }
-
+  
   class func blueTouch() {
     
     if ( Game.sharedInstance.buildStateMachine.currentState is BSPlaceItem ) {
@@ -75,11 +77,9 @@ class GameToolbar: HLToolbarNode {
     } else {
       Game.sharedInstance.gameStateMachine.enterState(GSBuild)
       Game.sharedInstance.buildStateMachine.enterState(BSPlaceItem)
-      
       Game.sharedInstance.placingObjectsQueue.append(ReceptionDesk)
       
     }
   }
-
 
 }
