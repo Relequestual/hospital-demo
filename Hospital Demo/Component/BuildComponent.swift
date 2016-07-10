c8d1d293714a65f0ffac773f0b6aac241a57096b
Ben Hutton <bh7@sanger.ac.uk>
2016-07-10 12:14:42 +0100

    WIP

---
+++ Hospital Demo/Component/BuildComponent.swift
@@ -0,0 +1,11 @@
+//
+//  BuildComponent.swift
+//  Hospital Demo
+//
+//  Created by Ben Hutton on 08/07/2016.
+//  Copyright © 2016 Ben Hutton. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
